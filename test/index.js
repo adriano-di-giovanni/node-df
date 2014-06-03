a4060e2e7e5af8ce0b409d829fc42efa930e428d
Adriano Di Giovanni <me@adrianodigiovanni.com>
2014-06-03 11:34:24 +0200

    add file option

--- test/index.js
+++ test/index.js
@@ -10,7 +10,7 @@ var
   df = require('../lib/df');
 
 describe('df', function () {
-  it('should do the job', function (done) {
+  it('! options', function (done) {
     df(function (error, response) {
       if (error) { return done(error); }
 
@@ -21,4 +21,21 @@ describe('df', function () {
       done();
     });
   });
+
+  it('options', function (done) {
+    var
+      options = {
+        file: '/'
+      };
+
+    df(options, function (error, response) {
+      if (error) { return done(error); }
+
+      assert.equal(_.isObject(response), true);
+
+      console.log(JSON.stringify(response, null, 2));
+
+      done();
+    });
+  })
 });
